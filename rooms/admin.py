62ccc32ad49d1de6ad186914631a7f0d8fdc80a2
hyowon <saedle91@gmail.com>
2020-02-25 22:51:59 +0900

    #6

--- rooms/admin.py
+++ rooms/admin.py
@@ -6,7 +6,7 @@ from . import models
 
 @admin.register(models.RoomType, models.Facility, models.Amenity, models.HouseRule)
 class ItemAdmin(admin.ModelAdmin):
-    
+
     """ Item Admin Definition """
 
     pass
@@ -17,7 +17,64 @@ class RoomAdmin(admin.ModelAdmin):
 
     """ Room Admin Definition """
 
-    pass
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")},
+        ),
+        ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths")},),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book")},),
+        (
+            "More About the Space",
+            {
+                "classes": ("collapse",),
+                "fields": ("amenities", "facilities", "house_rules"),
+            },
+        ),
+        ("Last Details", {"fields": ("host",)},),
+    )
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+        "count_amenities",
+    )
+
+    ordering = ("name", "price", "bedrooms")
+
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "host__gender",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "city",
+        "country",
+    )
+
+    search_fields = ("=city", "^host__username")
+
+    filter_horizontal = (
+        "amenities",
+        "facilities",
+        "house_rules",
+    )
+
+    def count_amenities(self, obj):
+        return "potato"
+
+    count_amenities.short_description = "hello sexy"
+
 
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
