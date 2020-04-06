4077b97393abfc2d0ed8e812c41b39cc91205d16
hyo <saedle91@gmail.com>
2020-04-07 00:05:39 +0900

    Revert "#10 views"
    This reverts commit 7422b80d0f3d70341df0d32aa655ae813abaad13.

--- rooms/views.py
+++ rooms/views.py
@@ -1,20 +1,7 @@
-from django.utils import timezone
-from django.views.generic import ListView
+from django.shortcuts import render
 from . import models
 
 
-class HomeView(ListView):
-
-    """ HomeView Definition"""
-
-    model = models.Room
-    paginate_by = 10
-    paginate_orphans = 5
-    ordering = "created"
-    context_object_name = "rooms"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        now = timezone.now()
-        context["now"] = now
-        return context
+def all_rooms(request):
+    all_rooms = models.Room.objects.all()
+    return render(request, "rooms/home.html", context={"rooms": all_rooms})
